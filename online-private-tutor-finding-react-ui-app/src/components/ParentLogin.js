211ff0008f1beb7fde80df3ac428eb75f7d15dd8
Shrutika <shrutika.taekar2000@gmail.com>
2021-01-21 10:04:24 +0530

    Parent

---
+++ online-private-tutor-finding-react-ui-app/src/components/ParentLogin.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import AddParent from './AddParent'
+
+export class ParentLogin extends Component {
+    render() {
+        return (
+            <div class="container mt-5 px-3 py-3 border border-dark rounded">
+            <div class="row">
+              <div class="col">
+                <h2>Parent Login</h2>
+                <br></br>
+                <form>
+    
+                  <div class="mb-3 row">
+                    <label for="username" class="col-sm-4 col-form-label">
+                      Username :
+                    </label>
+                    <div class="col-sm-5">
+                      <input
+                        type="text"
+                        class="form-control form-control-sm"
+                        name="username"
+                        ref={this.username}
+                        placeholder = "Eg: John"
+                        required
+                      />
+                    </div>
+                  </div>
+    
+                  <div class="mb-3 row">
+                    <label for="password" class="col-sm-4 col-form-label">
+                      Password :
+                    </label>
+                    <div class="col-sm-5">
+                      <input
+                        type="password"
+                        // pattern="(?=.\d)(?=.[a-z])(?=.*[A-Z]).{8,}"
+                        
+                        class="form-control form-control-sm"
+                        name="password"
+                        ref={this.password}
+                        placeholder="Eg: john@123"
+                        required
+                      />
+                    </div>
+                    </div>
+
+                    <div class="row mt-3">
+                <div class="col">
+                  <button
+                    class="btn btn-primary btn-sm ">
+                    Login
+                  </button>
+                </div>
+                
+                
+                </div>
+                <div class="row mt-3">
+                <div class="col">
+                <h5>or <a href={<AddParent />} class="link-primary">Registration</a></h5>
+                    </div>
+                    </div>
+            </form>
+          </div>
+        </div>
+
+            </div>
+        )
+    }
+}
+
+export default ParentLogin
---
+++ online-private-tutor-finding-react-ui-app/src/components/ViewTutor.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+
+export class ViewTutor extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             tutors: [],
+             message: ''
+        }
+    }
+    myfetch(){
+        // let URL ='http://localhost:8080/parent/viewAllTutor'
+        // axios.get(URL)
+        //     .then((res) => {
+        //         this.setState({
+        //             tutors: res.data
+        //         })
+        //         console.log(res.data)
+        //     })
+    }
+    componentDidMount()
+    {
+        this.myfetch()
+    }
+
+    
+    render() {
+        let tutorList= this.state.tutors.map((tutor , index) => {
+            return(
+                <tr key={index}>
+                <th>{tutor.tutorId}</th>
+                <td>{tutor.name}</td>
+                <td>{tutor.subject}</td>
+                <td>{tutor.phoneNumber}</td>
+                <td>{tutor.address}</td>
+                <td>{tutor.qualifications}</td>
+                <td>
+                        <button className="btn btn-primary mr-3">Request Demo</button>
+                        <button className="btn btn-primary">Book Tutor</button>
+                </td>
+                </tr>
+                )
+            })
+        return (
+            <div className="tutor-table-div">
+                <div>
+                    <h1>View Tutor</h1>
+                </div>
+                <div style={{marginLeft:'200px',marginRight:'200px'}}>
+                <table className="table table-warning tutor-table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Tutor Id</th>
+                            <th scope="col">Tutor Name</th>
+                            <th scope="col">Subject</th>
+                            <th scope="col">Tutor PhoneNumber</th>
+                            <th scope="col">Tutor Address</th>
+                            <th scope="col">Qualification</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tutorList}
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ViewTutor
