f418e3a582770af763b5bea6e8b6e1ad452d22c7
Shrutika <shrutika.taekar2000@gmail.com>
2021-01-21 10:35:18 +0530

    FirstComit

--- online-private-tutor-finding-react-ui-app/src/App.js
+++ online-private-tutor-finding-react-ui-app/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-<<<<<<< HEAD
+
 import ParentLogin from './components/ParentLogin';
 import ParentRouting from './components/ParentRouting';
-=======
+
 import AddUpdateTutor from './components/AddTutor';
 import ParentTable from './components/ParentTable';
 import TutorTable from './components/TutorTable';
@@ -13,21 +13,16 @@ import TutorRouting from './components/TutorRouting';
 
 import TutorViewEbook from './components/TutorViewEbook';
 import UpdateTutor from './components/UpdateTutor';
-<<<<<<< HEAD
 
-=======
->>>>>>> 8547e19496bde41c650eb91e90079132f0a99c1e
->>>>>>> cb95a86a91af41e4c45ddb915ffe635cd892ac7e
->>>>>>> c5767759943b1cf7f4136c377f137d90e8c35195
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <ParentRouting />
      <ParentLogin />
      
-=======
       {/* <AddUpdateTutor/> */}
       {/* <ParentTable/> */}
       {/* <TutorTable/> */}
@@ -42,11 +37,7 @@ function App() {
       {/* <AdminLogin/> */}
       {/* <TutorViewEbook/> */}
       {/* <UpdateTutor/> */}
-<<<<<<< HEAD
-=======
->>>>>>> 8547e19496bde41c650eb91e90079132f0a99c1e
->>>>>>> cb95a86a91af41e4c45ddb915ffe635cd892ac7e
->>>>>>> c5767759943b1cf7f4136c377f137d90e8c35195
+
     </div>
   );
 }
