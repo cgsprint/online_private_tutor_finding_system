1e3a46f5cfd96a96a8e171f60134644547efdbc2
Shrutika <shrutika.taekar2000@gmail.com>
2021-01-23 17:25:38 +0530

    ResponseEntity for Parent

--- online-private-tutor-finding-react-ui-app/src/actions/ParentActions.js
+++ online-private-tutor-finding-react-ui-app/src/actions/ParentActions.js
@@ -5,19 +5,19 @@ export const GET_ALL_TUTORS = 'GET_ALL_TUTORS'
 
 
 
-const addParentAction = (data) => {
+const addParentAction = (data,status) => {
     return {
         type: ADD_Parent,
-        data
+        data,
+        status
     }
 }
 export const addParent = (newParent) => {
     return (dispatch) => {
         axios.post(PARENT_BASE_URL + '/register', newParent)
             .then((response) => {
-                alert("Parent Successfully added")
-                //console.log(response.data)
-                dispatch(addParentAction(response.data))
+                console.log(response.data)
+                dispatch(addParentAction(response.data,response.status))
             })
     }
 }
--- online-private-tutor-finding-react-ui-app/src/stores/ParentReducer.js
+++ online-private-tutor-finding-react-ui-app/src/stores/ParentReducer.js
@@ -3,7 +3,8 @@ import * as actionCreated2 from '../actions/ParentViewTutorActions'
 
 const initialState = {
     returnedMessage: 'data not Found',
-    tutorsList: []
+    tutorsList: [],
+    status: 0
 }
 const parentReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -21,11 +22,13 @@ const parentReducer = (state = initialState, action) => {
         case actionCreators.ADD_Parent:
             let messageAfterAddition = action.data.message
             let listAfterAddition = action.data
+            let Reqstatus1 = action.status
             console.log('Addition of parent')
             console.log(listAfterAddition)
             console.log(messageAfterAddition)
             return {
-                returnedMessage: messageAfterAddition
+                returnedMessage: messageAfterAddition,
+                status : Reqstatus1
             }
             default:
             return state
