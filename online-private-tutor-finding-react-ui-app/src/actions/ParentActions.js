6cf37f54657b9fe21bb8ae9d69e7b01094e1d8e9
Meena Pallerla <meenapallerla143@gmail.com>
2021-01-23 12:31:32 +0530

    'ebook'

--- online-private-tutor-finding-react-ui-app/src/actions/ParentActions.js
+++ online-private-tutor-finding-react-ui-app/src/actions/ParentActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-export const ADD_Parent = 'ADD_Parent'
+export const ADD_Parent = 'ADD_Parent';
+export const GET_ALL_EBOOKS = 'GET_ALL_EBOOKS';
+export const GET_BOOKED_TUTOR = 'GET_BOOKED_TUTOR';
 
 const BASE_URL = 'http://localhost:8080/parent/'
 const addParentAction = (data) => {
@@ -15,6 +17,38 @@ export const addParent = (newParent) => {
                 alert("Parent Successfully added")
                 //console.log(response.data)
                 dispatch(addParentAction(response.data))
+            })
+    }
+}
+
+const getAllEbooksAction = (data) => {
+    return {
+        type: GET_ALL_EBOOKS,
+        data
+    }
+}
+export const getAllEbooks = () => {
+    return (dispatch) => {
+        axios.get(BASE_URL+ 'ebooks')
+            .then((response) => {
+                dispatch(getAllEbooksAction(response.data))
+                
+            })
+    }
+}
+
+const getBookedTutorAction = (data) => {
+    return {
+        type: GET_BOOKED_TUTOR,
+        data
+    }
+}
+export const getBookedTutor = () => {
+    return (dispatch) => {
+        axios.get(BASE_URL+ 'bookedtutor')
+            .then((response) => {
+                dispatch(getBookedTutorAction(response.data))
+                
             })
     }
 }
