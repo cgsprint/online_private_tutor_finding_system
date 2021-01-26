5e8255f3c380c6bb2eb0171b02d863235bcb565b
Chanchal Gupta <chanchalgupta397@gmail.com>
2021-01-26 17:20:28 +0530

    commit

--- online-private-tutor-finding-react-ui-app/src/components/AdminRouting.js
+++ online-private-tutor-finding-react-ui-app/src/components/AdminRouting.js
@@ -15,7 +15,7 @@ function AdminRouting() {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
--- online-private-tutor-finding-react-ui-app/src/components/ParentRouting.js
+++ online-private-tutor-finding-react-ui-app/src/components/ParentRouting.js
@@ -16,7 +16,7 @@ export default function ParentRouting() {
     return (
         <Router>
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
